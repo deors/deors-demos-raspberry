20015970446869788f6474cd54ac5a258154a5d3
jorge hidalgo <jorge.hidalgo@gmail.com>
2019-12-30 17:07:12 +0100

    add adafruit blinka example programs

---
+++ blinka/blinka-button-led.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import time
+import board
+import digitalio
+ 
+print("press the button!")
+ 
+led = digitalio.DigitalInOut(board.D23)
+led.direction = digitalio.Direction.OUTPUT
+ 
+button = digitalio.DigitalInOut(board.D4)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
+ 
+while True:
+    led.value = button.value # light when button is pressed!
