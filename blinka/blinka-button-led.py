76c4f899a919f7e4017bebfd9a7ab506a041388e
deors <jorge.hidalgo@gmail.com>
2020-02-10 20:26:01 +0100

    update button pin number

--- blinka/blinka-button-led.py
+++ blinka/blinka-button-led.py
@@ -3,15 +3,15 @@
 import time
 import board
 import digitalio
- 
+
 print("press the button!")
- 
+
 led = digitalio.DigitalInOut(board.D23)
 led.direction = digitalio.Direction.OUTPUT
- 
-button = digitalio.DigitalInOut(board.D4)
+
+button = digitalio.DigitalInOut(board.D5)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
- 
+
 while True:
     led.value = button.value # light when button is pressed!
