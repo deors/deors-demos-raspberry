c35e1f020e351ba74d7142dd7e4cdfa07c5c3c82
deors <jorge.hidalgo@gmail.com>
2020-04-04 21:55:39 +0200

    update 'termo' script to work with BME680 and Adafruit CircuitPython library

--- pystation/termo-avanzado.py
+++ pystation/termo-avanzado.py
@@ -1,161 +1,100 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import RPi.GPIO as GPIO
-import smbus
+import board
+import digitalio
+import busio
+import adafruit_bme680
 import time
-from ctypes import c_short
+import _thread
 
-AC = 23
-CALE = 24
+# leds
+led_ac = digitalio.DigitalInOut(board.D23)
+led_ac.direction = digitalio.Direction.OUTPUT
 
-def encender(pin):
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(pin, GPIO.OUT)
-  GPIO.output(pin, GPIO.HIGH)
-  return
+led_cale = digitalio.DigitalInOut(board.D24)
+led_cale.direction = digitalio.Direction.OUTPUT
 
-def apagar(pin):
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(pin, GPIO.OUT)
-  GPIO.output(pin, GPIO.LOW)
-  return
+def encender(led):
+  led.value = True
 
-def limpiar(pin):
-  GPIO.setwarnings(False)
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(pin, GPIO.OUT)
-  GPIO.cleanup()
-  return
+def apagar(led):
+  led.value = False
 
-DEVICE = 0x77 # Default device I2C address
+def limpiarLeds():
+  apagar(led_ac)
+  apagar(led_cale)
 
-#bus = smbus.SMBus(0) # Rev 1 Pi uses 0
-bus = smbus.SMBus(1) # Rev 2 Pi uses 1
+# sensor bme680
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
-def convertToString(data):
-  # Simple function to convert binary data into
-  # a string
-  return str((data[1] + (256 * data[0])) / 1.2)
+sensor.sea_level_pressure = 1013.25 #presión a nivel del mar (referencia)
 
-def getShort(data, index):
-  # return two bytes from data as a signed 16-bit value
-  return c_short((data[index] << 8) + data[index + 1]).value
+# teclado
+def input_thread(listener):
+    input()
+    listener.append(True)
 
-def getUshort(data, index):
-  # return two bytes from data as an unsigned 16-bit value
-  return (data[index] << 8) + data[index + 1]
+# función para procesar sigint (señal de parada)
+def sigint_handler(sig, frame):
+    limpiarLeds()
+    print()
+    sys.exit(0)
 
-def readBmp180Id(addr = DEVICE):
-  # Register Address
-  REG_ID     = 0xD0
+signal.signal(signal.SIGINT, sigint_handler)
 
-  (chip_id, chip_version) = bus.read_i2c_block_data(addr, REG_ID, 2)
-  return (chip_id, chip_version)
-
-def readBmp180(addr = DEVICE):
-  # Register Addresses
-  REG_CALIB  = 0xAA
-  REG_MEAS   = 0xF4
-  REG_MSB    = 0xF6
-  REG_LSB    = 0xF7
-  # Control Register Address
-  CRV_TEMP   = 0x2E
-  CRV_PRES   = 0x34
-  # Oversample setting
-  OVERSAMPLE = 3    # 0 - 3
-
-  # Read calibration data
-  # Read calibration data from EEPROM
-  cal = bus.read_i2c_block_data(addr, REG_CALIB, 22)
-
-  # Convert byte data to word values
-  AC1 = getShort(cal, 0)
-  AC2 = getShort(cal, 2)
-  AC3 = getShort(cal, 4)
-  AC4 = getUshort(cal, 6)
-  AC5 = getUshort(cal, 8)
-  AC6 = getUshort(cal, 10)
-  B1  = getShort(cal, 12)
-  B2  = getShort(cal, 14)
-  MB  = getShort(cal, 16)
-  MC  = getShort(cal, 18)
-  MD  = getShort(cal, 20)
-
-  # Read temperature
-  bus.write_byte_data(addr, REG_MEAS, CRV_TEMP)
-  time.sleep(0.005)
-  (msb, lsb) = bus.read_i2c_block_data(addr, REG_MSB, 2)
-  UT = (msb << 8) + lsb
-
-  # Read pressure
-  bus.write_byte_data(addr, REG_MEAS, CRV_PRES + (OVERSAMPLE << 6))
-  time.sleep(0.04)
-  (msb, lsb, xsb) = bus.read_i2c_block_data(addr, REG_MSB, 3)
-  UP = ((msb << 16) + (lsb << 8) + xsb) >> (8 - OVERSAMPLE)
-
-  # Refine temperature
-  X1 = ((UT - AC6) * AC5) >> 15
-  X2 = (MC << 11) / (X1 + MD)
-  B5 = X1 + X2
-  temperature = (B5 + 8) >> 4
-
-  # Refine pressure
-  B6  = B5 - 4000
-  B62 = B6 * B6 >> 12
-  X1  = (B2 * B62) >> 11
-  X2  = AC2 * B6 >> 11
-  X3  = X1 + X2
-  B3  = (((AC1 * 4 + X3) << OVERSAMPLE) + 2) >> 2
-
-  X1 = AC3 * B6 >> 13
-  X2 = (B1 * B62) >> 16
-  X3 = ((X1 + X2) + 2) >> 2
-  B4 = (AC4 * (X3 + 32768)) >> 15
-  B7 = (UP - B3) * (50000 >> OVERSAMPLE)
-
-  P = (B7 * 2) / B4
-
-  X1 = (P >> 8) * (P >> 8)
-  X1 = (X1 * 3038) >> 16
-  X2 = (-7357 * P) >> 16
-  pressure = P + ((X1 + X2 + 3791) >> 4)
-
-  return (temperature / 10.0, pressure / 100.0)
-
-def leerTemperaturaPresion():
-  (temperatura, presion) = readBmp180()
-  return (temperatura, presion)
-
+# main
 def main():
 
-  for i in range(0, 100):
-    # leemos la temperatura y la presión actuales
-    (temperatura, presion) = leerTemperaturaPresion()
+  print()
+  print("pulsa Enter para terminar")
 
-    print "Temperatura : ", temperatura, "C"
-    print "Presión     : ", presion, "mbar"
+  limpiarLeds()
 
-    if (temperatura > 27.0): # podemos cambiar la temperatura de corte
+  listener = []
+  _thread.start_new_thread(input_thread, (listener,))
+
+  while not listener:
+
+    # leemos la situación actual
+    temperatura = sensor.temperature
+    humedad = sensor.humidity
+    presion = sensor.pressure
+    altitud = sensor.altitude
+    gases = sensor.gas
+
+    # escribimos los valores medidos
+    print()
+    print("Temperatura : %0.1f C" % temperatura)
+    print("Humedad     : %0.1f %%" % humedad)
+    print("Presión     : %0.1f hPa" % presion)
+    print("Altitud     : %0.2f m" % altitud)
+    print("Gases       : %0.2f kΩ" % (gases / 1000))
+    print()
+
+    if (temperatura > 25.0): # podemos cambiar la temperatura de corte
       # hace calor
       # necesitamos el aire acondicionado
-      encender(AC)
-      apagar(CALE)
+      encender(led_ac)
+      apagar(led_cale)
+      print("--> temperatura alta")
     elif (temperatura < 21.0):
       # hace frío
       # necesitamos la calefacción
-      encender(CALE)
-      apagar(AC)
+      encender(led_cale)
+      apagar(led_ac)
+      print("--> temperatura baja")
     else:
       # ni frío ni calor
       # apagamos todo
-      apagar(AC)
-      apagar(CALE)
+      apagar(led_ac)
+      apagar(led_cale)
+      print("--> temperatura normal")
 
     time.sleep(1)
 
-  limpiar(AC)
-  limpiar(CALE)
+  limpiarLeds()
 
 if __name__ == "__main__":
    main()
