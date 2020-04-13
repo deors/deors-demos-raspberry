ca516b77777b1a4bbc609ea220dd3f81cc194b36
deors <jorge.hidalgo@gmail.com>
2020-04-13 16:30:16 +0200

    remove sea level pressure init (unneeded)

--- pystation/termo-680-plantilla.py
+++ pystation/termo-680-plantilla.py
@@ -29,8 +29,6 @@ def limpiarLeds():
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
-sensor.sea_level_pressure = 1013.25 #presión a nivel del mar (referencia)
-
 # teclado
 def input_thread(listener):
     input()
