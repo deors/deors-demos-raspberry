b61307fecb6e3dd1d0f334b7380826f9c4797425
deors <jorge.hidalgo@gmail.com>
2020-04-13 16:45:10 +0200

    add 'termo' scripts based on bmp280 sensor

---
+++ pystation/termo-basico.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import board
+import digitalio
+import busio
+import adafruit_bme280
+import time
+import signal
+import sys
+import _thread
+
+# leds
+led_ac = digitalio.DigitalInOut(board.D23)
+led_ac.direction = digitalio.Direction.OUTPUT
+
+led_cale = digitalio.DigitalInOut(board.D24)
+led_cale.direction = digitalio.Direction.OUTPUT
+
+def encender(led):
+  led.value = True
+
+def apagar(led):
+  led.value = False
+
+def limpiarLeds():
+  apagar(led_ac)
+  apagar(led_cale)
+
+# sensor bme280
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+# teclado
+def input_thread(listener):
+    input()
+    listener.append(True)
+
+# función para procesar sigint (señal de parada)
+def sigint_handler(sig, frame):
+    limpiarLeds()
+    print()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, sigint_handler)
+
+# main
+def main():
+
+  print()
+  print("pulsa Enter para terminar")
+
+  limpiarLeds()
+
+  listener = []
+  _thread.start_new_thread(input_thread, (listener,))
+
+  while not listener:
+
+    # leemos la situación actual
+    temperatura = sensor.temperature
+
+    # escribimos los valores medidos
+    print()
+    print("Temperatura : %0.1f C" % temperatura)
+    print()
+
+    if (temperatura > 25.0): # podemos cambiar la temperatura de corte
+      # hace calor
+      # necesitamos el aire acondicionado
+      encender(led_ac)
+      apagar(led_cale)
+      print("--> temperatura alta")
+    else:
+      # hace frío
+      # necesitamos la calefacción
+      encender(led_cale)
+      apagar(led_ac)
+      print("--> temperatura baja")
+
+    time.sleep(1)
+
+  limpiarLeds()
+
+if __name__ == "__main__":
+   main()
